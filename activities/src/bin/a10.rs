69932f247ca22a1a45d13855f140181516cad42f
Traian <traian30y@yahoo.co.uk>
2023-02-19 13:15:23 +0000

    a10

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -8,5 +8,36 @@
 //   is > 100 or <= 100
 // Use a function to print the messages
 // Use a match expression to determine which message to print
+use std::io;
 
-fn main() {}
+fn read_user_input() -> i32 {
+    println!("input an integer");
+    let mut my_input = String::new();
+    loop {
+        io::stdin()
+            .read_line(&mut my_input)
+            .expect("failed to read line.");
+        let my_input: i32 = match my_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input only numbers!");
+                my_input.clear();
+                continue;
+            }
+        };
+        return my_input;
+    }
+}
+
+fn comparison_fn(num: i32, comparison_result: bool) {
+    match comparison_result {
+        true => println!("The number {} is lower than 100", num),
+        false => println!("The number {} is greater/=than 100", num),
+    }
+}
+
+fn main() {
+    let my_num = read_user_input();
+    let comparison_result: bool = if my_num < 100 { true } else { false };
+    comparison_fn(my_num, comparison_result);
+}
