52999297f87554dff69e23bd91614b7dfb7d8f6d
Traian <traian30y@yahoo.co.uk>
2023-02-22 10:07:33 +0000

    a15_demo_2 demo advanced match

---
+++ activities/src/bin/a15_demo_2.rs
@@ -0,0 +1,35 @@
+// demo advanced match
+
+enum Discount {
+    Percent(i32),
+    Flat(i32),
+}
+
+struct Ticket {
+    event: String,
+    price: i32,
+}
+
+fn main() {
+    let n = 4;
+    match n {
+        3 => println!("three"),
+        other => println!("number: {:?}", other),
+    }
+
+    let flat = Discount::Flat(2);
+    match flat {
+        Discount::Flat(2) => println!("flat 2"),
+        Discount::Flat(amount) => println!("flat discount of: {:?}", amount),
+        _ => (),
+    }
+
+    let concert = Ticket {
+        event: "concert".to_owned(),
+        price: 60,
+    };
+    match concert {
+        Ticket { price: 50, event } => println!("price @50 of {:?} ", event),
+        Ticket { price, event } => println!("price: {:?}, event: {:?}", price, event),
+    }
+}
