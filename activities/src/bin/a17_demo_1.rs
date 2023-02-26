b1a4f083708ba6b1c367a7a5e889efcd33deca9a
Traian <traian30y@yahoo.co.uk>
2023-02-26 13:17:51 +0000

    a17 demo generating docs

--- activities/src/bin/a17_demo_1.rs
+++ activities/src/bin/a17_demo_1.rs
@@ -1,7 +1,19 @@
+/// A favourite color.
 enum Color {
     Red,
     Blue,
     Green,
+}
+
+// A piece of mail.
+struct Mail {
+    /// The destination address.
+    address: String,
+}
+
+/// Adds two numbers together.
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 fn main() {}
