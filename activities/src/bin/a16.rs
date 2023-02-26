e5ff543d253df4b718ace6926ed3a0d422fb0846
Traian <traian30y@yahoo.co.uk>
2023-02-26 02:15:47 +0000

    students vector lockersa16 modified

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -1,4 +1,4 @@
-// given a database of students(name, and locker number) print the name and locker nober for each student
+// given a database of students(name, and locker number) print the name and locker number for each student
 // if the student hasn't been assigned a locker no yet, ask the user for assigning a new locker no
 // the new locker no must be in range 1..300 and it shouldn't have been assignet yet
 use std::io;
@@ -29,26 +29,68 @@ fn validate_read_input() -> bool {
     }
 }
 
-fn read_locker() {}
+fn read_locker_number() -> u32 {
+    println!("please input a locker number!");
+    let mut input = String::new();
+    loop {
+        input.clear();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("could't read line!");
+        let input: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please input only integers between 0..300");
+                continue;
+            }
+        };
+        if input > 300 {
+            println!("Please input a number smaller than 300");
+            continue;
+        } else {
+            println!("input: {}", input);
+            return input;
+        }
+    }
+}
 
 fn main() {
     let mut students: Vec<StudentLockers> = Vec::new();
 
     students.push(StudentLockers {
         student_name: "Djibril".to_owned(),
-        locker_no: Some(7),
+        locker_no: Some(0),
     });
     students.push(StudentLockers {
         student_name: "Fane".to_owned(),
+        locker_no: Some(1),
+    });
+    students.push(StudentLockers {
+        student_name: "Lili".to_owned(),
         locker_no: Some(2),
+    });
+    students.push(StudentLockers {
+        student_name: "Vijelie".to_owned(),
+        locker_no: Some(3),
     });
     students.push(StudentLockers {
         student_name: "Spoitoru".to_owned(),
         locker_no: None,
     });
+    students.push(StudentLockers {
+        student_name: "Rene".to_owned(),
+        locker_no: None,
+    });
+    students.push(StudentLockers {
+        student_name: "Menolau".to_owned(),
+        locker_no: None,
+    });
 
     // creating a vector of current assigned locker numbers
     let mut current_assigned_lockers: Vec<u32> = Vec::new();
+    let mut new_students: Vec<StudentLockers> = Vec::new();
+
+    // create the current_assigned_lockers vector
     for student in &students {
         match student.locker_no {
             Some(locker) => current_assigned_lockers.push(locker),
@@ -56,26 +98,74 @@ fn main() {
         }
     }
 
-    // for mut student in students {
-    //     match student.locker_no {
-    //         Some(locker) => println!(
-    //             "{:?} has assigned locker: {:?}",
-    //             student.student_name, locker
-    //         ),
-    //         None => {
-    //             println!(
-    //                 "{:?} hasn't been assigned a locker yet.",
-    //                 student.student_name
-    //             );
-    //             println!("Do you want to assign a new locker number now? y/n");
-    //             let answer = validate_read_input();
-    //             match answer {
-    //                 true => {
-    //                     student.locker_no = Some(5);
-    //                 }
-    //                 false => (),
-    //             }
-    //         }
-    //     }
-    // }
+    for student in &students {
+        match student.locker_no {
+            Some(locker) => {
+                println!(
+                    "student: {} has assigned {} for locker",
+                    student.student_name, locker
+                );
+                new_students.push(StudentLockers {
+                    student_name: student.student_name.to_owned(),
+                    locker_no: Some(locker),
+                });
+            }
+            None => {
+                println!(
+                    "student: {} doesn't have assigned a locker",
+                    student.student_name
+                );
+                println!("Do you want to assign a new locker number now? y/n");
+                let answer = validate_read_input();
+                match answer {
+                    false => {
+                        new_students.push(StudentLockers {
+                            student_name: student.student_name.to_owned(),
+                            locker_no: None,
+                        });
+                    }
+                    true => loop {
+                        let new_locker = read_locker_number();
+                        let mut found_locker = false;
+                        for num in &current_assigned_lockers {
+                            if new_locker == *num {
+                                println!("{} already assigned!", num);
+                                found_locker = true;
+                                break;
+                            }
+                        }
+                        if found_locker == false {
+                            new_students.push(StudentLockers {
+                                student_name: student.student_name.to_owned(),
+                                locker_no: Some(new_locker),
+                            });
+                            current_assigned_lockers.push(new_locker);
+                            break;
+                        } else {
+                            continue;
+                        }
+                    },
+                }
+            }
+        }
+    }
+
+    println!("NEW MODIFIED STUDENTS VECTOR");
+
+    for student in &new_students {
+        match student.locker_no {
+            Some(locker) => {
+                println!(
+                    "student: {} has assigned {} for locker",
+                    student.student_name, locker
+                )
+            }
+            None => {
+                println!(
+                    "student: {} doesn't have assigned a locker",
+                    student.student_name
+                );
+            }
+        }
+    }
 }
