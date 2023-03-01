2d600eafd0ea5fe73a5381901609143668329ccb
Traian <traian30y@yahoo.co.uk>
2023-03-01 10:46:26 +0000

    a18_demo1

--- activities/src/bin/a18_demo1.rs
+++ activities/src/bin/a18_demo1.rs
@@ -1,7 +1,20 @@
+use std::io;
+
+#[derive(Debug)]
 enum MenuChoice {
     MainMenu,
     Start,
     Quit,
+}
+
+fn read_choice() -> String {
+    println!("please input choice: mainmenu/start/quit");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Couldn't read line.");
+    let input = input.trim();
+    input.to_owned()
 }
 
 fn get_choice(input: &str) -> Result<MenuChoice, String> {
@@ -13,4 +26,15 @@ fn get_choice(input: &str) -> Result<MenuChoice, String> {
     }
 }
 
-fn main() {}
+fn print_choice(choice: &Result<MenuChoice, String>) {
+    match choice {
+        Ok(result) => println!("valid choice: {:?}", result),
+        Err(error) => println!("error: {:?}", error),
+    }
+}
+
+fn main() {
+    let read_input = read_choice();
+    let choice = get_choice(&read_input);
+    print_choice(&choice);
+}
