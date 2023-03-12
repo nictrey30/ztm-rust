b4f26eba40e4031b90bc297ab9056b2a7fe75e63
Traian <traian30y@yahoo.co.uk>
2023-03-12 13:22:32 +0000

    a18

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -11,4 +11,37 @@
 // Return a result from the function
 // The Err variant should detail the reason why they cannot make a purchase
 
-fn main() {}
+use std::cmp::Ordering;
+
+struct Customer {
+    name: String,
+    age: i32,
+}
+
+fn restricted_purchase(customer: &Customer) -> Result<String, String> {
+    match customer.age.cmp(&21) {
+        Ordering::Less => Err("cannot buy, under 21y".to_owned()),
+        Ordering::Equal => Ok("can buy cigs".to_owned()),
+        Ordering::Greater => Ok("can buy cigs".to_owned()),
+    }
+}
+
+fn print_result(customer: &Customer, enquiry: &Result<String, String>) {
+    match enquiry {
+        Ok(result) => println!("{:?} {:?}", customer.name, result),
+        Err(error) => println!("{:?} {:?}", customer.name, error),
+    }
+}
+
+fn main() {
+    let customer_1 = Customer {
+        name: "Ionel".to_owned(),
+        age: 32,
+    };
+    let customer_2 = Customer {
+        name: "Gigel".to_owned(),
+        age: 17,
+    };
+    print_result(&customer_1, &restricted_purchase(&customer_1));
+    print_result(&customer_2, &restricted_purchase(&customer_2));
+}
